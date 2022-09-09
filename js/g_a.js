cb5cce226ce4b392925a87efc465ad0cdc0303ce
Ali Paşa Turhan <87146626+alipasaturhan@users.noreply.github.com>
2022-09-09 04:34:23 +0300

    Add files via upload

--- js/g_a.js
+++ js/g_a.js
@@ -1,8 +1,17 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-HG8JQ59PYY"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+Promise.all([
+    new Promise((resolve) => {
+        var script = document.createElement('script');
+        script.src = "https://www.googletagmanager.com/gtag/js?id=G-HG8JQ59PYY";
+        script.async = true;
+        document.body.appendChild(script);
+        resolve();
+    }),
+    new Promise((resolve) => {
+        gtag('js', new Date());
 
-  gtag('config', 'G-HG8JQ59PYY');
-</script>
+        gtag('config', 'G-HG8JQ59PYY');
+        resolve();
+    })
+]);
